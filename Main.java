a418c795f2da78b206587905881060a7e4f166aa
ychasser <41200686+ychasser@users.noreply.github.com>
2025-08-29 23:30:30 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,90 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("les nombres particuliers");
+
+    
+  }
+
+  /**
+   * fonction estDiviseur
+   * données :  
+   * résulat :  
+   */
+
+
+  /**
+   * fonction somDivNonTriviau
+   * données :  
+   * résultat : 
+   */
+ 
+
+  /**
+   * fonction somDivPropres
+   * données : 
+   * résultat : 
+   */
+
+
+  /**
+   * fonction estPremier
+   * données : 
+   * résultat : 
+   */
+ 
+
+  /**
+   * fonction verifNbPremier
+   * données : 
+   * résultats : 
+   */
+
+
+  /**
+   * fonction estParfait
+   * données : 
+   * résultat : 
+   */
+
+
+  /**
+   * fonction verifNbParfait
+   * données : 
+   * résultat :
+   */
+
+
+  /**
+   * fonction estChanceux
+   * données : 
+   * résultat : 
+   */
+
+
+  /**
+   * fonction verifNbChanceux
+   * données : 
+   * résultat : 
+   */
+
+
+  /**
+   * fonction sontAmiables
+   * données : 
+   * résultat : 
+   */
+
+  /**
+   * fonction monAmi
+   * données : 
+   * résulat : 
+   */
+
+ /**
+   * fonction verifNbCAmiables
+   * données : 
+   * résultat : 
+   */
+
+
+}
